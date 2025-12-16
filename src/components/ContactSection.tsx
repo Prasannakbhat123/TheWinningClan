e8fd7b78b961933f37acbe7c9d370db93f5c28ef
Prasannakbhat123 <prasannabhat@gmail.com>
2025-12-16 21:30:47 +0530

    email

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -29,7 +29,7 @@ const ContactSection = () => {
                 <svg className="w-5 h-5 text-[#f4d35e] shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <p className="text-lg font-semibold text-[#f4d35e]">Email</p>
+                <p className="text-lg font-semibold text-[#f4d35e]">thewinningclan11@gmail.com</p>
               </div>
             </a>
             <a
